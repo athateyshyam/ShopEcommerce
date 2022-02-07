86a0519b5e138b57d1b9c14c2bb5e1d23aaebe15
athateyshyam <athateyshyam@gmail.com>
2022-02-07 18:14:51 +0530

    Code new user function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
@@ -2,8 +2,11 @@ package com.shopme.admin.service;
 
 import java.util.List;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 public interface UserService {
 	public List<User> listAll();
+	public List<Role>listRoles();
+	public void save(User user);
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/UserServiceImpl.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/UserServiceImpl.java
@@ -5,18 +5,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.admin.repository.RoleRepository;
 import com.shopme.admin.repository.UserRepository;
 import com.shopme.admin.service.UserService;
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 
 	@Override
 	public List<User> listAll() {
-		return (List<User>) repository.findAll();
+		return (List<User>) userRepository.findAll();
+	}
+
+	@Override
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepository.findAll();
+	}
+
+	@Override
+	public void save(User user) {
+		userRepository.save(user);
 	}
 
 }
